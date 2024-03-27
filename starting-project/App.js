0736b649c3aaac3c3e0ce9996c309acc04cb1cc1
roxanap33 <roxana.ciocian@principal33.com>
2024-03-27 15:04:19 +0200

    20: First layout of the page

--- starting-project/App.js
+++ starting-project/App.js
@@ -1,28 +1,33 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummytext}>Hello World!!!!</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button title="Add Goal" />
       </View>
-      <Text style={styles.dummytext}>Another text</Text>
-      <Button title="TAP" />
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 50,
   },
-  dummytext: {
-    margin: 63,
-    borderWidth: 2,
-    borderColor: "red",
-    padding: 15,
+
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
   },
 });
