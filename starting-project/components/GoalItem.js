4405daca790ea77041b3785a61e5d875716e7c72
roxanap33 <roxana.ciocian@principal33.com>
2024-03-27 22:19:47 +0200

    30: Split code into components and use props

---
+++ starting-project/components/GoalItem.js
@@ -0,0 +1,21 @@
+import { StyleSheet, View, Text } from "react-native";
+
+export default function GoalItem({ text }) {
+  return (
+    <View style={styles.goalItem}>
+      <Text style={styles.goalText}>{text}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    padding: 8,
+  },
+  goalText: {
+    color: "white",
+  },
+});
